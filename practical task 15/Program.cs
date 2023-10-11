d94db483bfe4a5b727baa64ccb05219bb1415d20
AlexandrKern <137929102+AlexandrKern@users.noreply.github.com>
2023-10-11 12:45:45 +0300

    Добавьте файлы проекта.

---
+++ practical task 15/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Multithreading
+{
+    internal class Program
+    {
+        static void Flow()
+        {
+            var flow = Thread.CurrentThread;
+            flow.Name = "flow";
+            for (int i = 0; i < 30; i++)
+            {
+                Console.Write("* ");
+                Thread.Sleep(100);
+            }
+            Console.WriteLine($"Конец работы потока {flow.Name}");
+        }
+        static void Main(string[] args)
+        {
+            var mayThread = Thread.CurrentThread;
+            mayThread.Name = "mayThread";
+            Thread thread = new Thread(Flow);
+            thread.Start();
+            for (int i = 0; i < 30; i++)
+            {
+                Console.Write("0 ");
+                Thread.Sleep(200);
+            }
+            Console.WriteLine($"Конец работы потока {mayThread.Name}");
+        }
+    }
+}
